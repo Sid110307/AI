6ec278017a0fab8d462cd7a361549a5a908a1ab9
Sid110307 <srsfool@gmail.com>
2023-04-12 15:06:51 +0530

    Added GradeSchoolMath dataset

--- src/include/datasets.h
+++ src/include/datasets.h
@@ -50,7 +50,7 @@ public:
 
 			if (curl)
 			{
-				FILE* file = fopen("cornell_movie_dialogs_corpus.zip", "wb");
+				FILE* file = fopen("./cornell_movie_dialogs_corpus.zip", "wb");
 				curl_easy_setopt(curl, CURLOPT_URL,
 								 "https://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip");
 				curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
@@ -61,10 +61,17 @@ public:
 
 				CURLcode res = curl_easy_perform(curl);
 				if (res != CURLE_OK)
+				{
 					std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+					exit(EXIT_FAILURE);
+				}
 
 				curl_easy_cleanup(curl);
 				fclose(file);
+			} else
+			{
+				std::cerr << "curl_easy_init() failed." << std::endl;
+				exit(EXIT_FAILURE);
 			}
 
 			std::cout << "Download complete. Extracting..." << std::endl;
@@ -85,20 +92,34 @@ public:
 						if (file)
 						{
 							std::ofstream out(DATASET_ROOT + "/movie_lines.txt", std::ios::binary);
+							if (!out.good())
+							{
+								std::cerr << "Failed to open output file." << std::endl;
+								exit(EXIT_FAILURE);
+							}
+
 							char buffer[1024];
 							int bytesRead;
 
 							while ((bytesRead = (int) zip_fread(file, buffer, 1024)) > 0) out.write(buffer, bytesRead);
 							out.close();
 							zip_fclose(file);
+						} else
+						{
+							std::cerr << "Failed to open file in zip archive." << std::endl;
+							exit(EXIT_FAILURE);
 						}
 					}
 				}
 
 				zip_close(archive);
+			} else
+			{
+				std::cerr << "Failed to open zip archive." << std::endl;
+				exit(EXIT_FAILURE);
 			}
 
-			std::cout << "Extraction complete." << std::endl;
+			std::cout << "Extraction complete. Deleting temporary files..." << std::endl;
 
 			std::filesystem::rename(DATASET_ROOT + "/cornell movie-dialogs corpus/movie_lines.txt",
 									DATASET_ROOT + "/movie_lines.txt");
@@ -189,4 +210,106 @@ public:
 	}
 };
 
-// TODO: Add more datasets
+class GSMDataset : public Dataset
+{
+public:
+	void validate() override
+	{
+		auto writeCallback = [](char* contents, size_t size, size_t nmemb, FILE* buffer)
+		{
+			return fwrite(contents, size, nmemb, buffer);
+		};
+
+		std::chrono::high_resolution_clock::time_point start = printDebug(
+				"Checking if " + DATASET_ROOT + "/train_socratic.jsonl exists...");
+		if (!std::filesystem::exists(DATASET_ROOT + "/train_socratic.jsonl"))
+		{
+			std::cout << "The file " << DATASET_ROOT << "/train_socratic.jsonl does not exist. Downloading..."
+					  << std::endl;
+			CURL* curl = curl_easy_init();
+			if (curl)
+			{
+				FILE* file = fopen("./train_socratic.jsonl", "wb");
+				curl_easy_setopt(curl, CURLOPT_URL,
+								 "https://raw.githubusercontent.com/openai/grade-school-math/master/grade_school_math/data/train_socratic.jsonl");
+				curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+				curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+				curl_easy_perform(curl);
+				curl_easy_cleanup(curl);
+				fclose(file);
+			}
+
+			std::filesystem::rename("train_socratic.jsonl", DATASET_ROOT + "/train_socratic.jsonl");
+		}
+
+		printDebug("Done in " + std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(
+				std::chrono::high_resolution_clock::now() - start).count()) + "ms.", false);
+	}
+
+	std::vector<std::string> load() override
+	{
+		auto start = printDebug("Loading the GMS dataset...");
+		std::vector<std::string> lines;
+
+		std::ifstream file(DATASET_ROOT + "/train_socratic.jsonl");
+		if (!file.is_open())
+		{
+			std::cerr << "The file " << DATASET_ROOT << "/train_socratic.jsonl could not be opened." << std::endl;
+			exit(EXIT_FAILURE);
+		}
+
+		std::string line;
+		while (std::getline(file, line)) lines.push_back(line);
+
+		file.close();
+		printDebug("Done in " + std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(
+				std::chrono::high_resolution_clock::now() - start).count()) + "ms.", false);
+
+		return lines;
+	}
+
+	std::vector<std::string> preprocess(const std::vector<std::string> &lines) override
+	{
+		auto preprocessLine = [&](const std::string &_line)
+		{
+			std::string text = _line;
+			std::smatch match;
+
+			while (std::regex_search(text, match, std::regex(R"(\*\* (.+?) \*\*)")))
+			{
+				std::string question = match[1].str();
+				std::string answer = question.substr(question.find("#### ") + 5);
+				question = question.substr(0, question.find("#### "));
+				text = replace(text, match[0].str(), question + " " + answer);
+			}
+
+			return text;
+		};
+
+		auto start = printDebug("Preprocessing the lines...");
+		std::vector<std::string> conversations;
+
+		for (const auto &line: lines)
+		{
+			std::string text = preprocessLine(line);
+			text = text.substr(text.find("#### ") + 5);
+			conversations.push_back(text);
+		}
+
+		std::vector<std::string> _lines;
+
+		ThreadPool pool;
+		for (const auto &line: conversations)
+		{
+			printDebug("Preprocessing the line: " + line, false);
+			pool.queueJob([&]() { _lines.push_back(preprocessLine(line)); });
+		}
+
+		pool.stop();
+		printDebug("Done in " + std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(
+				std::chrono::high_resolution_clock::now() - start).count()) + "ms.", false);
+
+		return _lines;
+	}
+};
+
